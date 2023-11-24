92bf68ced0db4f225c61997dc05a37f0ad553909
Rohit Chandramouli <rsc4@illinois.edu>
2023-11-24 13:35:33 -0600

    Add files via upload

---
+++ GW170817-Recovery/xibar_GW170817_binarylove.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+
+import sys
+# sys.path.append("../../../NRTidal-D")
+import bilby
+import nrtidal_d
+import numpy as np
+from astropy.coordinates import SkyCoord
+from gwpy.timeseries import TimeSeries
+
+#-----------------------------------------------------------------
+
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-o", "--outdir", type=str, default="outdir_GW170817_4",
+                    help="Output directory for run.")
+
+parser.add_argument("-l", "--label", type=str, default="bilby_GW170817_4",
+                    help="Label of the bilby run.")
+
+parser.add_argument("-n", "--npool", type=int, default=1, 
+                    help="Number of CPUs.")
+
+parser.add_argument("-sd", "--strain_dir", type=str, 
+default = "/Users/abhi/Work/Projects/BDNK-Critical-Collapse/Data-Analysis/Cluster-Data/scratch/GW170817_files/Strain-Data-GW170817/no-glitch",
+help="Location of strain data")
+
+
+args = parser.parse_args()
+
+#-----------------------------------------------------------------
+
+bilby.core.utils.setup_logger(outdir=args.outdir, label=args.label, log_level = "debug")
+logger = bilby.core.utils.logger
+
+#-----------------------------------------------------------------
+trigger_time = 1187008882.43
+
+roll_off = 0.2  # Roll off duration of tukey window in seconds
+
+# 4096 seconds of data
+
+duration = 128   # Analysis segment duration
+start_time = 1187008755
+end_time = start_time + duration
+
+sampling_frequency = 4096
+#-----------------------------------------------------------------
+
+
+hdf5_filenames = {
+    "H1": args.strain_dir+ "/H-H1_LOSC_CLN_4_V1-1187007040-2048_no_glitch.hdf5",
+    "L1": args.strain_dir+ "/L-L1_LOSC_CLN_4_V1-1187007040-2048_no_glitch.hdf5",
+    "V1": args.strain_dir+ "/V-V1_LOSC_CLN_4_V1-1187007040-2048_no_glitch.hdf5"
+}
+
+datapsd=np.loadtxt(args.strain_dir + '/GWTC1_GW170817_PSDs.dat')
+
+farray=datapsd[:,0]
+h1psd = datapsd[:,1]
+l1psd= datapsd[:,2]
+v1psd = datapsd[:,3]
+
+psd_array = {"H1":h1psd, "L1":l1psd, "V1": v1psd}
+
+det_names = np.array(["H1,L1,V1"])
+ifo_list = bilby.gw.detector.InterferometerList([])
+
+#Setting Strain Data
+for det in ["H1", "L1", "V1"]:
+    logger.info("Loading data for ifo {}".format(det))
+    ifo = bilby.gw.detector.get_empty_interferometer(det)
+    data = TimeSeries.read(
+        hdf5_filenames[det], start=start_time,
+        end=end_time, format="hdf5.gwosc"
+    )
+    ifo.strain_data.set_from_gwpy_timeseries(data)
+    logger.info("Loading psd data for ifo {}".format(det))
+    freq = farray
+    psd = psd_array[det]
+    logger.info("Setting IFO PSD from loaded PSD.")
+    ifo.power_spectral_density = bilby.gw.detector.PowerSpectralDensity(
+        frequency_array=freq, psd_array=psd)
+    ifo_list.append(ifo)
+
+logger.info("Finished setting up strain and PSD.")
+logger.info("Saving IFO data plots to {}".format(args.outdir))
+bilby.core.utils.check_directory_exists_and_if_not_mkdir(args.outdir)
+ifo_list.plot_data(outdir=args.outdir, label=args.label)
+#-----------------------------------------------------------------
+
+# injection_parameters = dict(
+#         mass_1=1.51,mass_2=1.26,a_1=0.02, a_2=0.02, tilt_1=1.10, tilt_2=1.20,
+#         phi_12=3.17, phi_jl=3.25, luminosity_distance=40., theta_jn=2.63, psi=1.83,
+#         phase=1.3, geocent_time=trigger_time, ra=3.35, dec= -0.29,lambda_1 =500,
+#         lambda_2 = 350
+# )
+
+#-------------------------------------------------------------
+priors = bilby.gw.prior.PriorDict()
+priors['mass_1'] = bilby.gw.prior.Constraint(minimum=1, maximum=2,
+                                             name='mass_1', latex_label='$m_1$', unit=None)
+priors['mass_2'] = bilby.gw.prior.Constraint(minimum=1, maximum=2,
+                                             name='mass_2', latex_label='$m_2$', unit=None)
+
+priors['chirp_mass'] = bilby.gw.prior.UniformInComponentsChirpMass(minimum=1.184, maximum=1.25, name='chirp_mass', 
+                                              latex_label='$\\mathcal{M}$', unit=None)
+priors['mass_ratio'] = bilby.gw.prior.UniformInComponentsMassRatio(minimum=0.5, maximum=1, name='mass_ratio',
+                                              latex_label='$q$', unit=None)
+
+priors['a_1'] = bilby.gw.prior.Uniform(name='a_1', minimum=0, maximum=0.05,
+                                       latex_label='$a_1$', unit=None, boundary=None)
+priors['a_2'] = bilby.gw.prior.Uniform(name='a_2', minimum=0, maximum=0.05,
+                                       latex_label='$a_2$', unit=None, boundary=None)
+
+priors['tilt_1'] = bilby.prior.Sine(name='tilt_1', latex_label='$\\theta_1$', unit=None)
+priors['tilt_2'] = bilby.prior.Sine(name='tilt_2', latex_label='$\\theta_2$', unit=None)
+priors['phi_12'] = bilby.gw.prior.Uniform(name='phi_12', minimum=0, maximum=2 * np.pi,
+                                          boundary='periodic', latex_label='$\\Delta\\phi$', unit=None)
+priors['phi_jl'] = bilby.gw.prior.Uniform(name='phi_jl', minimum=0, maximum=2 * np.pi,
+                                          boundary='periodic', latex_label='$\\phi_{JL}$', unit=None)
+priors['luminosity_distance'] = bilby.gw.prior.UniformSourceFrame(name='luminosity_distance',
+                                                                     minimum=10, maximum=100, latex_label='$d_L$',
+                                                                     unit='Mpc', boundary=None)
+priors['phase'] = bilby.core.prior.Uniform(name='phase', minimum=0, maximum=2 * np.pi, boundary='periodic')
+priors['theta_jn'] = bilby.prior.Sine(name='theta_jn', latex_label='$\\theta_{JN}$',
+                                         unit=None, minimum=0, maximum=np.pi, boundary=None)
+priors['psi'] = bilby.gw.prior.Uniform(name='psi', minimum=0, maximum=np.pi, boundary='periodic',
+                                       latex_label='$\\psi$', unit=None)
+
+priors["lambda_1"] = bilby.core.prior.Constraint( name="lambda_1", minimum=0, maximum=3000)
+priors["lambda_2"] = bilby.core.prior.Constraint(name="lambda_2", minimum=0, maximum=3000)
+priors["lambda_s"] = bilby.core.prior.Triangular(name="lambda_s", mode=1500,minimum = 0, maximum = 3000,latex_label='$\\Lambda_s$')
+# priors["lambda_tilde"] = bilby.core.prior.Uniform(0, 5000, name="lambda_tilde")
+# priors["delta_lambda_tilde"] = bilby.core.prior.Uniform(
+#     -500, 1000, name="delta_lambda_tilde")
+
+
+priors['geocent_time'] = bilby.core.prior.Uniform(
+    minimum=trigger_time - 0.2,
+    maximum=trigger_time + 0.2,
+    name='geocent_time',
+    latex_label='$t_c$',
+    unit='$s$'
+)
+priors['dec'] =  bilby.prior.Cosine(name='dec', latex_label='$\\mathrm{DEC}$',
+                                       unit=None, minimum=-np.pi / 2, maximum=np.pi / 2, boundary=None)
+priors['ra'] =  bilby.gw.prior.Uniform(name='ra', minimum=0, maximum=2 * np.pi, boundary='periodic',
+                                       latex_label='$\\mathrm{RA}$', unit=None)
+
+priors["xi_bar"] = bilby.core.prior.Uniform(0,1000,name="xi_bar")
+
+#for key in ['chi_1','chi_2']:
+#    priors.pop(key)
+#-----------------------------------------------------------------
+
+waveform_arguments = dict(
+    waveform_approximant="IMRPhenomPv2_NRTidal",
+    reference_frequency=20.0
+)
+
+waveform_generator = bilby.gw.WaveformGenerator(
+    duration=duration,
+    sampling_frequency=sampling_frequency,
+    frequency_domain_source_model=nrtidal_d.source_binary_love,
+    parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_neutron_star_parameters,
+    waveform_arguments=waveform_arguments
+)
+
+#-----------------------------------------------------------------
+likelihood = bilby.gw.GravitationalWaveTransient(
+    interferometers=ifo_list, waveform_generator=waveform_generator,
+    time_marginalization=False, phase_marginalization=True,
+    distance_marginalization=False, priors=priors)
+
+#-----------------------------------------------------------------
+import sampler_params
+sampler_parameters = sampler_params.sampler_params()
+
+assert(sampler_parameters["sampler"]=="dynesty")
+
+result = bilby.run_sampler(
+        likelihood=likelihood, 
+        priors=priors, 
+        sampler="dynesty", 
+        sample = 'rwalk',
+        bound = 'live',
+        nlive=1500, 
+        nact=10, 
+        dlogz=0.01, 
+        maxmcmc=5000,
+        check_point_delta_t=sampler_parameters["check_point_delta_t"],
+        npool=args.npool, 
+        outdir=args.outdir, label=args.label,
+        conversion_function=bilby.gw.conversion.generate_all_bns_parameters)
+
+result.plot_corner()
+
